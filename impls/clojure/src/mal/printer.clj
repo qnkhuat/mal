51c632e240c551ac2ce49489026baffdfdb2e380
Earther <qn.khuat@gmail.com>
2021-06-04 13:03:55 +0700

    cleaner handlign for map

--- impls/clojure/src/mal/printer.clj
+++ impls/clojure/src/mal/printer.clj
@@ -6,10 +6,9 @@
   (cond 
     (vector? l) (str "[" (clojure.string/join " " (map pr-str l)) "]")
     (list? l) (str "(" (clojure.string/join " " (map pr-str l)) ")")
-    (map? l) (str "{" (clojure.string/join " " (doseq [[k v] l] (str (pr-str k) " " (pr-str v))) "}") ; REFINE THIS
+    (map? l) (str "{" (clojure.string/join " " (map (fn [[k v]] (str (pr-str k) " " (pr-str v))) l)) "}")
     (symbol? l) l
     (number? l) l
     (boolean? l) (str l)
     true (str "\"" l "\"")
   ))
-
